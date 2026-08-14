4a881634b5d431307188ffb98e6b8c1ff4ac38e8
Lalit Kumar <lalitbgs2584@gmail.com>
2026-08-14 20:32:11 +0530

    feat: initialize user-mobile app with Supabase, TanStack Query, and database schema setup

---
+++ packages/db/index.ts
@@ -0,0 +1,16 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | unknown
+}
+
+const adapter = new PrismaPg({
+    connectionString:process.env.DIRECT_URL,
+})
+
+export const db = globalForPrisma.prisma || new PrismaClient({
+    adapter:adapter,
+})
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
